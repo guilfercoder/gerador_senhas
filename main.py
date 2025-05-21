01645d2eb86ac92cc718f6fd20302481de2d42df
Guilherme Ferreira <guilherme-53@hotmail.com>
2025-05-21 20:49:13 -0300

    Adicionando bibliotecas e variáveis

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ while True:
     qntd_letras =  int(input("Digite a quantidade de letras da senha: "))
     qntd_numeros = int(input("Digite a quantidade de numeros da senha: "))
     qntd_especiais = int(input("Digite a quantidade de especiais da senha: "))
+
+    soma = qntd_letras + qntd_numeros + qntd_especiais
+
+    if soma != tamanho:
+        print(f"\n ERRO! A soma {soma}, não corresponde ao informado {tamanho}.")
+        print("Por favor, tente novamente.")
+    else:
+        letras = random.choices(string.ascii_letters, k=qntd_letras)
+        numeros = random.choices(string.digits, k=qntd_numeros)
+        especiais = random.choices(string.ascii_letters, k=qntd_especiais)
+
+
