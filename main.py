eefbcef2210b9cd32e0186f1660fb1b71211d841
Guilherme Ferreira <guilherme-53@hotmail.com>
2025-05-21 20:44:58 -0300

    Adicionando variaveis

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("Hello World!")
+import random
+import string
+
+print("========Gerador de Senhas do Guizão========")
+
+while True:
+
+    tamanho = int(input("Digite o tamanho da senha: "))
+
+    qntd_letras =  int(input("Digite a quantidade de letras da senha: "))
+    qntd_numeros = int(input("Digite a quantidade de numeros da senha: "))
+    qntd_especiais = int(input("Digite a quantidade de especiais da senha: "))
